f12fcc77898139403241374d1f96421a53c8f77e
Lari Liuhamo <lari.liuhamo@gmail.com>
2026-05-27 01:41:03 +0300

    feat: Migrate to uv and switch to dynamic versioning

--- src/escapyde/__init__.py
+++ src/escapyde/__init__.py
@@ -1,4 +1,11 @@
 """A library for simplifying ANSI escape sequences in Python."""
 
+import importlib
+
 from escapyde.ansi import *
 from escapyde.colours import *
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
